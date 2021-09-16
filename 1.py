a5102d1f21e81f547082cafbe3c0d05bf7340bb1
Shirley Huang <yqhuang@ygomi.com>
2021-09-16 22:51:11 +0800

    1 done

---
+++ 1.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/two-sum/
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(0, len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+print(Solution().twoSum([2, 7, 11, 15], 9))
